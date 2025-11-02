466bd1faa126d2f9f6def57a356b9928ed6fcec2
Maxim Eremenko <eremenkomax@gmail.com>
2025-11-02 23:43:34 +0300

    Second

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	appService := service.New()
+	appHandlers := handlers.New(appService)
+	srv := server.New(logger, appHandlers)
+
+	if err := srv.Run(); err != nil {
+		logger.Fatalf("server stopped with error: %s", err)
+	}
 }
